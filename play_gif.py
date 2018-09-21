17fa005fe1d76c674e3b305d57cc37fa99d4f6d2
Victor Cabral <vccabral@gmail.com>
2018-09-20 22:58:16 -0400

    new fiel

--- play_gif.py
+++ play_gif.py
@@ -1,15 +1,17 @@
 from PIL import Image
 from neopixel import *
 from itertools import product
+import sys
 
 # LED strip configuration:
-LED_COUNT      = 750      # Number of LED pixels.
+LED_COUNT      = int(sys.argv[2])      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+DOWNLED = int(sys.argv[3])
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
@@ -37,7 +39,7 @@ while True:
             for x in range(0, resized.size[0]):
                 for y in range(0, resized.size[1]):
                     r, g, b = resized.getpixel((x, y))
-                    strip.setPixelColor(plane_to_zig_zagged_line(x, y), Color(r//25, g//25, b//25))
+                    strip.setPixelColor(plane_to_zig_zagged_line(x, y), Color(r//DOWNLED, g//DOWNLED, b//DOWNLED))
             strip.show()
 
     except EOFError:
