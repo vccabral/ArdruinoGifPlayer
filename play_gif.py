c5c0402736b6d56b5f0eb7ab8705be469494fd53
Victor Cabral <vccabral@gmail.com>
2018-09-20 23:14:54 -0400

    new fiel

--- play_gif.py
+++ play_gif.py
@@ -4,14 +4,16 @@ from itertools import product
 import sys
 
 # LED strip configuration:
-LED_COUNT      = int(sys.argv[1])      # Number of LED pixels.
+LED_COUNT      = 750      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-DOWNLED = int(sys.argv[2])
+DOWNLED = int(sys.argv[1])
+DOWNLED = int(sys.argv[1])
+DOWNLED = int(sys.argv[1])
 
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
@@ -34,7 +36,8 @@ while True:
     try:
         while 1:
             im.seek(im.tell()+1)
-            resized = im.resize((30,30)).convert('RGB')
+            cropped = im.crop((0, 0, 100, 100))
+            resized = cropped.resize((30,30)).convert('RGB')
 
             for x in range(0, resized.size[0]):
                 for y in range(0, resized.size[1]):
